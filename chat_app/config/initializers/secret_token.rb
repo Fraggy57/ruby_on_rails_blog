32d4243c42ba8e979461212c438ea4c5d32f5e23
Your Name <you@example.com>
2013-05-11 15:20:07 +0300

    Chat basic

---
+++ chat_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FayeTutorial::Application.config.secret_token = '2e385d456a8fce673460e762de50321727eb44d07a7b89d97d2f8f65e6917c94af673aa00f3c9e06e3520a74059828b9b27f1703fb55b1d9eb3f5600a8f14c68'
